06da2741ada7d29abbcda96f987ba6b04f206060
StefanPatman <stefanpatman91@gmail.com>
2022-03-15 23:29:28 +0200

    Add processor history endpoints

--- src/processor/processor.py
+++ src/processor/processor.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 import logging
 from argparse import ArgumentParser
 from requests import post
@@ -6,58 +6,87 @@ from time import time, sleep
 from math import sin, pi
 from random import uniform
 from hashlib import sha1
+from collections import deque
 from os import getenv
-
 
 
 class Processor:
     def __init__(self):
+        self.history_length = 10
         self.elements = {}
         self.locations = {}
+
     def listen(self, id, data):
         if self.elements.get(id) is None:
-            self.elements[id] = []
+            self.elements[id] = deque(maxlen=self.history_length)
             self.locations[id] = data['location']
         self.elements[id].append(data)
-        # print(self.elements)
-    def list_id(self, id):
+
+    def exists(self, id):
+        return (int(id) in self.elements.keys())
+
+    def history(self, id):
         data = self.elements.get(int(id))
         if data is None:
-            return "No such id"
-        return jsonify(data)
-    def list_location(self, location):
-        data = [self.elements[id] for id in self.locations.keys()
-                if self.locations[id]==location]
-        return jsonify(data)
-
+            return f"No such id: {id}"
+        return list(data)
 
 a = Processor()
 app = Flask(__name__)
 
+
 @app.route('/listen', methods = ['POST'])
 def listen():
-    logging.info('Enter')
     data = request.get_json()
-    data['timestamp_processed'] = time()
+    data['timestamp_arrived'] = time()
     id = int(data['id'])
-    print(data)
     a.listen(id, data)
-    # print(a.elements)
     return {}
 
-@app.route('/data', methods = ['POST','GET'])
-def data():
-    id = request.args.get('id')
-    location = request.args.get('location')
-    if id is not None:
-        return a.list_id(id)
-    elif location is not None:
-        return a.list_location(location)
-    return "error"
+@app.route('/history/all', methods = ['GET'])
+def history_all():
+    app.logger.info(f'All history')
+    return jsonify({
+        id: a.history(id) for id in a.elements.keys()
+    })
 
+@app.route('/history/generator', methods = ['GET'])
+def history_generator():
+    ids = request.args.getlist('id')
+    app.logger.info(f'History for IDs: {repr(ids)}')
+    if ids is None:
+        return "Missing argument: id"
+    return jsonify({
+        id: a.history(id) for id in ids
+    })
+
+@app.route('/graph', methods = ['GET'])
+def graph():
+    id = request.args.get('id')
+    app.logger.info(f'Graph for ID: {repr(id)}')
+    if id is None:
+        return "Missing argument: id"
+    if not a.exists(id):
+        return f"No such id: {id}"
+    return jsonify({
+        x['timestamp_last_generated']: x['temperature']
+        for x in a.history(id)
+    })
+
+#
+# /latest/all
+# /latest/floor?id=1&id=2
+# /latest/generator?id=1
+# /maximum/all
+# /maximum/floor?id=1&id=2
+# /maximum/generator?id=1
+# /history/all
+# /history/generator?id=1
+# /history/graph?id=1
+#
 
 def main(port):
-    app.run(port=port, host='0.0.0.0', debug=False)
+    app.run(port=port, host='0.0.0.0', debug=True)
 
 
 if __name__ == '__main__':
